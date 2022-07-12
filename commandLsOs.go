31e5dd5fe9671524075bee200026276b12bb23ec
Frank Taubert <ftaubert@casee.de>
2022-07-12 13:01:18 +0200

    update actions

--- commandLsOs.go
+++ commandLsOs.go
@@ -1,35 +1,36 @@
 package main
 
 import (
-	"fmt"
-	client "github.com/Virtomize/uii-go-api"
-	"os"
-	"text/tabwriter"
+    "fmt"
+    client "github.com/Virtomize/uii-go-api"
+    "os"
+    "text/tabwriter"
 )
 
 type lsOsCommand struct {
 }
 
+// Run executes the command to get a list of operating systems.
 func (l *lsOsCommand) Run(ctx *context) error {
-	c, err := client.NewClient(ctx.token)
-	if err != nil {
-		return err
-	}
+    c, err := client.NewClient(ctx.token)
+    if err != nil {
+        return err
+    }
 
-	osList, err := c.OperatingSystems()
-	if err != nil {
-		return err
-	}
-	w := new(tabwriter.Writer)
+    osList, err := c.OperatingSystems()
+    if err != nil {
+        return err
+    }
+    w := new(tabwriter.Writer)
 
-	w.Init(os.Stdout, 8, 8, 1, '\t', 0)
-	defer w.Flush()
+    w.Init(os.Stdout, 8, 8, 1, '\t', 0)
+    defer w.Flush()
 
-	fmt.Fprintf(w, "\n %s\t%s\t%s\t%s\t", "Displayname", "Distribution", "Version", "Architecture")
-	for _, osEntry := range osList {
-		fmt.Fprintf(w, "\n %s\t%s\t%s\t%s\t", osEntry.DisplayName, osEntry.Distribution, osEntry.Version, osEntry.Architecture)
-	}
-	fmt.Fprintf(w, "\n")
+    fmt.Fprintf(w, "\n %s\t%s\t%s\t%s\t", "Displayname", "Distribution", "Version", "Architecture")
+    for _, osEntry := range osList {
+        fmt.Fprintf(w, "\n %s\t%s\t%s\t%s\t", osEntry.DisplayName, osEntry.Distribution, osEntry.Version, osEntry.Architecture)
+    }
+    fmt.Fprintf(w, "\n")
 
-	return nil
+    return nil
 }
